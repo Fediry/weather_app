eb3244492d4d5fe4ddcabec832013df8b9f7b8eb
John Nolan <90651514+Fediry@users.noreply.github.com>
2023-11-29 00:10:48 -0800

    Added a basic frame for the flask app in app.py. Simple testing. Needs to be completed.

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+from weather import get_grid_location, get_weather
+from location import gps
 from flask import Flask
 
+# app = Flask(__name__)
+
+local_coordinates = gps.get_current_gps_coordinates()
+print(local_coordinates)
+local_weather = get_weather(get_grid_location(local_coordinates))
+print(local_weather)
