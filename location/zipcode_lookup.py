4ca34851476379dc5fa18da660291edd9cad452e
John Nolan <90651514+Fediry@users.noreply.github.com>
2023-12-09 00:36:38 -0800

    Tinkered with zipcode_lookup.py to try to add City to the return results, but was too buggy.

--- location/zipcode_lookup.py
+++ location/zipcode_lookup.py
@@ -1,5 +1,6 @@
 import csv
 from dataclasses import dataclass
+import geocoder
 import pandas as pd
 
 @dataclass
@@ -18,16 +19,25 @@ def get_coordinates_from_zipcode(zipcode):
     data = load_zipcodes()
     zip_lookup = data[data['ZIP'] == zipcode]
     coordinates = (zip_lookup['LAT'].iloc[0], zip_lookup['LNG'].iloc[0])
+
     return coordinates
+    # city = city_lookup(coordinates)
+    # return city, coordinates
 
 def load_zipcodes():
     """ Load the CSV file of Zip Codes to Latitude and Longitude into a Pandas Data Frame and return it.
+        (Local copy of US Census data for zip codes to lattitude/longitude.)
     """
     FILE = 'location/USZip2GPS.txt'
     data = pd.read_csv(FILE)
     return data
 
-
+# def city_lookup(coordinates):
+#     """ Takes lattitude and longitude as a tuple and does a reverse lookup to find the city.
+#         (Could have used a striaght forward lookup with this api, but decided to use the local copy of US Census data)
+#     """
+#     g = geocoder.osm(coordinates, method='reverse')
+#     return g.city
 
 if __name__ == '__main__':
     zipcode = '95126'
