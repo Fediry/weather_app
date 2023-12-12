de4073d4089a9c6025119e83826c63e2eccb2e55
John Nolan <90651514+Fediry@users.noreply.github.com>
2023-12-12 14:37:45 -0800

    Rearranged the directory structure to make more sense and added an error check to zipcode_lookup.py.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from weather import get_grid_location, get_weather
-from location import gps, zipcode_lookup
+from src import weather, gps, zipcode_lookup
 from flask import Flask, flash, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
@@ -12,19 +11,21 @@ def format_weather_periods():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        zipcode = request.form['zipcode']
+        zipcode = request.form['zip code']
 
         if not zipcode:
-            flash('Please enter your zipcode.')
+            flash('Please enter your zip code.')
+        if len(zipcode) < 1 or len(zipcode) > 5:
+            flash('Please enter a valid, 5-digit zip code.')
         else:
             location_info = zipcode_lookup.get_coordinates_from_zipcode(zipcode)
             latitude, longitude = location_info
-            local_weather = get_weather(get_grid_location((latitude, longitude)))
+            local_weather = weather.get_weather(weather.get_grid_location((latitude, longitude)))
             return render_template('index.html', city=zipcode, latitude=latitude, longitude=longitude, local_weather=local_weather)
 
 
     location_info = gps.get_current_gps_coordinates()
     city, coordinates = location_info
     latitude, longitude = coordinates
-    local_weather = get_weather(get_grid_location(coordinates))
+    local_weather = weather.get_weather(weather.get_grid_location(coordinates))
     return render_template('index.html', city=city, latitude=latitude, longitude=longitude, local_weather=local_weather)
--- src/zipcode_lookup.py
+++ src/zipcode_lookup.py
@@ -1,43 +1,36 @@
-import csv
 from dataclasses import dataclass
-import geocoder
 import pandas as pd
 
 @dataclass
 class ZipcodeCoordinates:
-    #City?
     zipcode: str 
     latitude: str
     longitude: str
 
 def get_coordinates_from_zipcode(zipcode):
-    """ Takes a zipcode, finds it in the dataframe and returns a tuple of latitude and longitude.
+    """ Takes a zip code, finds it in the dataframe and returns a tuple of latitude and longitude.
     """
-    if type(zipcode) is str:
-        zipcode = int(zipcode)
+    
+    try:
+        if type(zipcode) is str:
+            zipcode = int(zipcode)
+    except ValueError as e:
+        print(f"Error: {e}, zipcode should be 5-digit str.")
     
     data = load_zipcodes()
     zip_lookup = data[data['ZIP'] == zipcode]
     coordinates = (zip_lookup['LAT'].iloc[0], zip_lookup['LNG'].iloc[0])
 
     return coordinates
-    # city = city_lookup(coordinates)
-    # return city, coordinates
 
 def load_zipcodes():
     """ Load the CSV file of Zip Codes to Latitude and Longitude into a Pandas Data Frame and return it.
         (Local copy of US Census data for zip codes to lattitude/longitude.)
     """
-    FILE = 'location/USZip2GPS.txt'
+    FILE = 'data/USZip2GPS.txt'
     data = pd.read_csv(FILE)
     return data
 
-# def city_lookup(coordinates):
-#     """ Takes lattitude and longitude as a tuple and does a reverse lookup to find the city.
-#         (Could have used a striaght forward lookup with this api, but decided to use the local copy of US Census data)
-#     """
-#     g = geocoder.osm(coordinates, method='reverse')
-#     return g.city
 
 if __name__ == '__main__':
     zipcode = '95126'
